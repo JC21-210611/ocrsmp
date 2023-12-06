f8df191ddf380c375322ccf6cdb3b33252b4bdba
JK3A15 <210611@jc_21.jp>
2023-12-06 12:30:30 +0900

    セーブがトリガーになっているがAPIに画像は持ってこれる

--- lib/post.dart
+++ lib/post.dart
@@ -6,8 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-String val = "neko";
-
 class StatePostPage extends StatefulWidget{
   const StatePostPage(this.image, {Key? key}) : super(key: key);
   final XFile image;
@@ -19,6 +17,33 @@ class StatePostPage extends StatefulWidget{
 
 class PostPage extends State<StatePostPage> {
   //const PostPage({Key? key}) : super(key: key);
+  static String val = "neko";
+
+  void postData(XFile img) async {
+    print("パス：" + img.path);
+    File file = File(img.path);
+    // 画像のバイナリデータを読み込みます.
+    ByteData data = await file.readAsBytes().then((bytes) =>
+        ByteData.sublistView(Uint8List.fromList(bytes)));
+    List<int> imageBytes = data.buffer.asUint8List(); // 修正
+
+    var response = await http
+        .post(Uri.parse('https://r05-jk3a15cognitive.cognitiveservices.azure.com/computervision/imageanalysis:analyze?language=ja&api-version=2023-02-01-preview&features=read'),
+        headers: {
+          'Ocp-Apim-Subscription-Key':'2a4e99f274a14a94a4b5f26077b97cf0',
+          'Content-Type':'application/octet-stream',
+        },
+        body: imageBytes
+    );
+    Map<String, dynamic> jsonDataMap = json.decode(response.body);
+    // "content"フィールドの値を取得
+    String content = jsonDataMap['readResult']['content'];
+    print('Content: $content');
+
+    setState(() {
+      val = content;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,41 +57,14 @@ class PostPage extends State<StatePostPage> {
               Image.file(File(path.join(widget.image.path))),
               TextButton(
                   onPressed: () {
-                    setState(() {
                       postData(widget.image);
-                    });
                   },
                   child: Text('上の画像をAPIに送信します')),
               Text(val,style:TextStyle(fontSize: 15))
             ],
           ),
         ),
-
       ),
     );
   }
-}
-
-void postData(XFile img) async {
-  print("パス：" + img.path);
-  File file = File(img.path);
-  // 画像のバイナリデータを読み込みます.
-  ByteData data = await file.readAsBytes().then((bytes) =>
-          ByteData.sublistView(Uint8List.fromList(bytes)));
-  List<int> imageBytes = data.buffer.asUint8List(); // 修正
-
-  var response = await http
-      .post(Uri.parse('https://r05-jk3a15cognitive.cognitiveservices.azure.com/computervision/imageanalysis:analyze?language=ja&api-version=2023-02-01-preview&features=read'),
-      headers: {
-        'Ocp-Apim-Subscription-Key':'2a4e99f274a14a94a4b5f26077b97cf0',
-        'Content-Type':'application/octet-stream',
-      },
-      body: imageBytes
-  );
-  Map<String, dynamic> jsonDataMap = json.decode(response.body);
-  // "content"フィールドの値を取得
-  String content = jsonDataMap['readResult']['content'];
-  print('Content: $content');
-
-  val = content;
 }
